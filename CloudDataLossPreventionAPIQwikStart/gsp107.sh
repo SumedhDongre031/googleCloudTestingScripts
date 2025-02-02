9d947716671e87d0cc993afc79ecf184467c20c5
Sumedh Dongre <69501694+SumedhDongre031@users.noreply.github.com>
2025-02-03 02:59:38 +0530

    gsp107

--- CloudDataLossPreventionAPIQwikStart/gsp107.sh
+++ CloudDataLossPreventionAPIQwikStart/gsp107.sh
@@ -1,28 +1,5 @@
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
 
-BG_BLACK=`tput setab 0`
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-BG_YELLOW=`tput setab 3`
-BG_BLUE=`tput setab 4`
-BG_MAGENTA=`tput setab 5`
-BG_CYAN=`tput setab 6`
-BG_WHITE=`tput setab 7`
-
-BOLD=`tput bold`
-RESET=`tput sgr0`
-#----------------------------------------------------start--------------------------------------------------#
-
-echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
-
-export PROJECT_ID=$DEVSHELL_PROJECT_ID
+gcloud auth list
 
 cat > inspect-request.json <<EOF_END
 {
@@ -47,17 +24,20 @@ cat > inspect-request.json <<EOF_END
 }
 EOF_END
 
-gcloud auth print-access-token
+
+ACCESS_TOKEN=$(gcloud auth application-default print-access-token)
 
 curl -s \
-  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+  -H "Authorization: Bearer $ACCESS_TOKEN" \
   -H "Content-Type: application/json" \
-  https://dlp.googleapis.com/v2/projects/$PROJECT_ID/content:inspect \
+  https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/content:inspect \
   -d @inspect-request.json -o inspect-output.txt
 
-cat inspect-output.txt
 
 gsutil cp inspect-output.txt gs://$DEVSHELL_PROJECT_ID-bucket
+
+
+
 
 cat > new-inspect-file.json <<EOF_END
 {
@@ -83,16 +63,12 @@ cat > new-inspect-file.json <<EOF_END
 }
 EOF_END
 
+
+
 curl -s \
   -H "Authorization: Bearer $(gcloud auth print-access-token)" \
   -H "Content-Type: application/json" \
-  https://dlp.googleapis.com/v2/projects/$PROJECT_ID/content:deidentify \
+  https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/content:deidentify \
   -d @new-inspect-file.json -o redact-output.txt
 
-cat redact-output.txt
-
 gsutil cp redact-output.txt gs://$DEVSHELL_PROJECT_ID-bucket
-
-echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
-
-#-----------------------------------------------------end----------------------------------------------------------#
